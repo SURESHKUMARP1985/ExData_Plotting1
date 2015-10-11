5b1b8acb357453542dd056c2915ccf42916bf80e
SURESHKUMARP1985 <sureshmalar@gmail.com>
2015-10-11 10:07:16 +0530

    Plot1
    Plot1 R program

---
+++ plot1.R
@@ -0,0 +1,14 @@
+#The program read the data and plots a histogram
+
+# Reads data from file and extracts required data
+
+inputDataFile<-"household_power_consumption.txt"
+readData <- read.table(inputDataFile, header=TRUE, sep=";")
+requiredData <- readData[readData$Date  %in% c("1/2/2007","2/2/2007") ,]
+
+# Plot the histogram
+
+globalActPower <- as.numeric(requiredData$Global_active_power)
+png("plot1.png", width=480, height=480)
+hist(globalActPower, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+dev.off()
